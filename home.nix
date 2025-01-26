def9660547cf28d3cd39027a499fb38add124123
Damien Dumontet <damien.dumontet@protonmail.com>
2025-01-26 13:04:52 -0300

    update

--- home.nix
+++ home.nix
@@ -288,7 +288,7 @@
 
     [language-server.eslint]
     args = ["--stdio"]
-    command = "vscode-eslint-language-server"
+    command = "/Users/lnk0/.yarn/bin/eslint-lsp"
 
     [language-server.vscode-json-language-server.config]
     json = { validate = { enable = true }, format = { enable = true } }
